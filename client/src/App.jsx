564b7dbbf505801f01919ce09dcaae451c528f52
Aiman Rosli <aimanrosli977@yahoo.com>
2024-07-01 13:03:38 +0800

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,25 @@
-import { useState } from 'react'
+import { BrowserRouter, Routes, Route  } from "react-router-dom"
+import Home from './pages/Home'
+import About from "./pages/About"
+import Search from "./pages/Search"
+import SignIn from "./pages/SignIn"
+import SignUp from "./pages/SignUp"
+import Dashboard from "./pages/Dashboard"
+
 
 function App() {
 
   return (
-    <h1 className='text-3xl font-bold underline'>
-      Hello World!
-    </h1>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/about" element={<About />}/> 
+        <Route path="/sign-in" element={<SignIn />}/> 
+        <Route path="/sign-up" element={<SignUp />}/> 
+        <Route path="/search" element={<Search />}/>  
+        <Route path="/dashboard" element={<Dashboard />}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
