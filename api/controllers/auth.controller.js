c7897a6c33c3760570894160aa043985ea82b08b
Aiman Rosli <aimanrosli977@yahoo.com>
2024-07-02 01:02:50 +0800

    Add middleware and a function to handle errors, Do some code cleanup

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,27 +1,36 @@
 import User from "../models/user.model.js";
-import bcryptjs from 'bcryptjs';
+import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
 
+/**
+ * Handle signUp
+ */
 export const signup = async (req, res, next) => {
-    const {username, email, password} = req.body;
+  const { username, email, password } = req.body;
 
-    if(!username || !email || !password || username === ''
-        || email === '' || password === ''){
-            next(errorHandler(400, 'All fields are required'));
-    }
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    email === "" ||
+    password === ""
+  ) {
+    next(errorHandler(400, "All fields are required"));
+  }
 
-    const hashedPassword = bcryptjs.hashSync(password, 10);
+  const hashedPassword = bcryptjs.hashSync(password, 10);
 
-    const newUser = new User({
-        username,
-        email,
-        password:hashedPassword,
-    });
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
 
-    try{
-        await newUser.save();
-        res.json('Signup successful');
-    } catch (error){
-        next(error);
-    }
+  try {
+    await newUser.save();
+    res.json("Signup successful");
+  } catch (error) {
+    next(error);
+  }
 };
--- api/index.js
+++ api/index.js
@@ -1,26 +1,55 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import userRoutes from './routes/user.route.js';
-import authRoutes from './routes/auth.route.js';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
+import authRoutes from "./routes/auth.route.js";
 
+/**
+ * Configure .env file to store secret key
+ */
 dotenv.config();
 
-mongoose.connect(process.env.MONGO).then(() => {
-    console.log('MongoDb is connected');
-}).catch(err => {
+/**
+ * Connect with mongoDb
+ */
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDb is connected");
+  })
+  .catch((err) => {
     console.log(err);
-});
+  });
 
 const app = express();
 
+/**
+ * Enable json
+ */
 app.use(express.json());
 
+/**
+ * Enable server on port 3000
+ */
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-})
+  console.log("Server is running on port 3000");
+});
 
-app.use('/api/user', userRoutes);
-app.use('/api/auth', authRoutes);
+/**
+ * API routes
+ */
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
 
-
+/**
+ *  Middleware on handle error
+ */
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
